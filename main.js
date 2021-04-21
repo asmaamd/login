767b42f4680a96e4f1cf1bfd2f34aaa5ca0337d5
Otmane <82597916+otmaaane@users.noreply.github.com>
2021-04-21 10:07:54 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,47 @@
-const electron = require('electron')
-const app = electron.app
-var path = require('path')
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const { remote } = require('electron')
+const localStorage = require('localStorage')
+const app = electron.app;
+var path = require('path');
+
+const BrowserWindow = electron.BrowserWindow;
+var loginWindow
+app.on('ready', function() {
+
+    loginWindow = new BrowserWindow({
+        width: 450,
+        height: 600,
+        maxheight: 600,
+        maxwidth: 300,
+
+        // CONSTRUIRE WINDOW PERSONNALISE
+        frame: false,
+        titleBarStyle: "hidden",
+
+        // resize
+        resizable: false,
+        maximizable: false,
+        backgroundColor: '#2f4f4f',
+
+    })
+
+    loginWindow.loadURL('file://' + __dirname + '/html/login.html')
+
+    /* loginWindow.once('ready-to-show',function(){
+         loginWindow.show()
+     })*/
+    loginWindow.webContents.openDevTools();
+    /*   ipcMain.on('asynchronous-message', (event, arg) => {
+            console.log(arg)
+        })*/
+    //localStorage.setItem("test", loginWindow);
+   
 
 
-app.on('ready',function(){
-    let loginWindow = new BrowserWindow({
-        width:450,
-        height:600,
-        maxHeight: 600,
-        maxWidth : 600,
+})
 
-        frame : false,
-        titleBarStyle :"hidden",
-        resizable : false,
-        backgroundColor :'#B0C4DE'
 /*
-        webPreferences :{ 
-            preload : path.join(__dirname,'preload.js'),
-            nodeIntegration :true
-        } */
-    })
-    loginWindow.loadURL('file://'+__dirname+'/html/login.html')
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') app.quit()
 
-    loginWindow.once('ready_to_show',function(){
-        loginWindow.show()
-    })
-
-
-
-})
-
-app.on('window-all-closed',function(){
-    if(process.platform !=='darwin') app.quit()
-})
+})*/
